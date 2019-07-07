0294defebe91e3312caee73afb96b7c5e27aec8d
kholilboy <45350583+kholilboy@users.noreply.github.com>
2019-07-07 21:15:25 +0700

    Update 2019_07_05_135957_t_admin.php

--- database/migrations/2019_07_05_135957_t_admin.php
+++ database/migrations/2019_07_05_135957_t_admin.php
@@ -9,10 +9,11 @@ class TAdmin extends Migration
     public function up()
     {
         Schema::create('t_admin', function (Blueprint $kolom){
-            $kolom->increments('id');
+            $kolom->increments('id_adm');
             $kolom->char('u_adm');
             $kolom->char('p_adm');
-            $kolom->timestamps();
+            $kolom->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $kolom->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));        
         });
     }
 
