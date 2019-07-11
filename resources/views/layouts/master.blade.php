2c010a7980c878f22108a023ee64a3e6b9af1585
kholilboy <45350583+kholilboy@users.noreply.github.com>
2019-07-11 21:21:08 +0700

    Create master.blade.php

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 2 | Blank Page</title>
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="stylesheet" href="public/admin/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="public/admin/bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="public/admin/bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="public/admin/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="public/admin/css/skins/_all-skins.min.css">
+
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+<!-- Site wrapper -->
+<div class="wrapper">
+
+</html>
