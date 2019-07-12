fc8edf6a2b3f46d40f7e693bfe01b3bc47caa318
kholilboy <45350583+kholilboy@users.noreply.github.com>
2019-07-12 23:26:13 +0700

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -102,5 +102,21 @@
                 <!-- inner menu: contains the actual data -->
                 <ul class="menu">
 
+                  <li><!-- Task item -->
+                    <a href="#">
+                      <h3>
+                        Design some buttons
+                        <small class="pull-right">20%</small>
+                      </h3>
+                      <div class="progress xs">
+                        <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar"
+                             aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
+                          <span class="sr-only">20% Complete</span>
+                        </div>
+                      </div>
+                    </a>
+                  </li>
+                  <!-- end task item -->
+
 
 </html>
