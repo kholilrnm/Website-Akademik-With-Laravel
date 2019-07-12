3c08b303b16bd1525b33481b0b15c3fd34c5ae17
kholilboy <45350583+kholilboy@users.noreply.github.com>
2019-07-12 23:27:41 +0700

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -127,5 +127,11 @@
           </li>
           <!-- User Account: style can be found in dropdown.less -->
 
+          <li class="dropdown user user-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <img src="public/admin/img/user2-160x160.jpg" class="user-image" alt="User Image">
+              <span class="hidden-xs">Alexander Pierce</span>
+            </a>
+
 
 </html>
