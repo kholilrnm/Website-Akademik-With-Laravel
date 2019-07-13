df2c5d899362490538aba2b012aa01c5fb821d5a
kholilboy <45350583+kholilboy@users.noreply.github.com>
2019-07-13 20:46:38 +0700

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -159,5 +159,17 @@
                 </div>
                 <!-- /.row -->
               </li>
+                            <!-- Menu Footer-->
+              <li class="user-footer">
+                <div class="pull-left">
+                  <a href="#" class="btn btn-default btn-flat">Profile</a>
+                </div>
+                <div class="pull-right">
+                  <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                </div>
+              </li>
+            </ul>
+          </li>
+
 
 </html>
