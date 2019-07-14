c175d89db5173378faaee97082cf278377de7436
kholilboy <45350583+kholilboy@users.noreply.github.com>
2019-07-14 23:31:51 +0700

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -171,5 +171,15 @@
             </ul>
           </li>
 
+          <!-- Control Sidebar Toggle Button -->
+          <li>
+            <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+  <!-- =============================================== -->
 
 </html>
