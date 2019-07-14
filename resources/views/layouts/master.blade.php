b231534e9a0aff0dadb1961164f34649d4ca83e1
kholilboy <45350583+kholilboy@users.noreply.github.com>
2019-07-14 23:33:32 +0700

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -191,6 +191,11 @@
           <img src="public/admin/img/user2-160x160.jpg" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
+          <p>Alexander Pierce</p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+      <!-- search form -->
 
 
 
