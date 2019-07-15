5f232db2fbea0f832e2d9d6e3eec7f62637a79e0
kholilboy <45350583+kholilboy@users.noreply.github.com>
2019-07-15 23:22:38 +0700

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -200,7 +200,10 @@
         <div class="input-group">
           <input type="text" name="q" class="form-control" placeholder="Search...">
           <span class="input-group-btn">
-      
+                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                </button>
+              </span>
+        </div>
 
 
 
