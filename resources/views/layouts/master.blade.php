5040d71532688bb432aac51ef060c470516b8ebc
kholilboy <45350583+kholilboy@users.noreply.github.com>
2019-07-15 23:24:02 +0700

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -207,6 +207,8 @@
       </form>
       <!-- /.search form -->
       <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">MAIN NAVIGATION</li>
 
 
 
