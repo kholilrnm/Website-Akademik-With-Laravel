cdf25a7e65de0e8d10dba392bb210dab3fc79f58
kholilboy <45350583+kholilboy@users.noreply.github.com>
2019-07-18 20:00:53 +0700

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -209,6 +209,9 @@
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
         <li class="header">MAIN NAVIGATION</li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
 
 
 
