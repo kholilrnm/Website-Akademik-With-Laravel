1ae293257684996af2ec8b2e56d9a315956cdabc
kholilboy <45350583+kholilboy@users.noreply.github.com>
2019-07-18 20:01:16 +0700

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -212,6 +212,10 @@
         <li class="treeview">
           <a href="#">
             <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
 
 
 
