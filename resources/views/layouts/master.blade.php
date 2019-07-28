08cfcc4207318848b21ee542b16e3ac8c2d0dca8
kholilboy <45350583+kholilboy@users.noreply.github.com>
2019-07-28 22:26:58 +0700

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -232,4 +232,7 @@
           <ul class="treeview-menu">
             <li><a href="../../index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
             <li><a href="../../index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+            </ul>
+        </li>
+        <li class="treeview">
 </html>
