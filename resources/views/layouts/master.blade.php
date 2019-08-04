547439bd388b21ab54188eca4eebaaa528a5b614
kholilboy <45350583+kholilboy@users.noreply.github.com>
2019-08-04 19:25:35 +0700

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -37,3 +37,12 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </a>
+
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <!-- Messages: style can be found in dropdown.less-->
+          <li class="dropdown messages-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <i class="fa fa-envelope-o"></i>
+              <span class="label label-success">4</span>
+            </a>
